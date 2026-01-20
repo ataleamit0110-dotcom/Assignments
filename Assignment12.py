438aac156f6332ae2fb9c2d041626e2d04a9bcfc
Amit Maruti Atale <>
2026-01-21 01:16:16 +0530

    Assignment 12 completed

--- Assignment12.py
+++ Assignment12.py
@@ -15,3 +15,39 @@ def checkVowels(ch):
     
 result = checkVowels('a')
 print("Is vowel:", result)
+
+# 2 WAP which accept 1 number and it print its factors
+def printFactors(no):
+    for i in range(1, no+1):
+        if no % i == 0:
+            print("factors are ",i)
+
+printFactors(12)
+
+# 3.WAP which accept 2 nos and print addition, multiplication, division and subtraction of that nos
+def arithmeticOperations(no1, no2):
+    addition = no1 + no2
+    subtraction = no1 - no2
+    multiplication = no1 * no2
+    division = no1 / no2
+    return addition, subtraction, multiplication, division
+
+add, sub, mul, div = arithmeticOperations(10, 5)
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+
+#4 def print 1 to n nos
+def printNos(no):
+    for value in range(1, no+1):
+        print(value)
+
+printNos(10)
+
+#5 def print 1 to n in reverse
+def printNosReverse(no):
+    for value in range(no, 0, -1):
+        print(value)
+
+printNosReverse(10)
