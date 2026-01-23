8bbb5eb263ed7f1e5ac53656a3fcc2458fbf5c5a
Amit Maruti Atale <>
2026-01-24 01:26:02 +0530

    update code

--- Assignment14.py
+++ Assignment14.py
@@ -2,45 +2,48 @@
 
 # def square(num):
 #     return num * num
-
+# 1. write a lambda function which accepts one number and return square of that number
 square = lambda num: num * num
 print("Square is:", square(5))
 
-
+# 2. write a lambda function which accepts one number and return cube of that number
 cube = lambda num : num * num * num
 print("Cube is:", cube(3))
 
+# 3. write a lambda function which accepts two numbers and return greater number
 max = lambda no1, no2 : no1 if no1> no2 else no2
 print("Greater number is:", max(10, 20))
 print("Greater number is:", max(100, 20))
 
-
+# 4. write a lambda function which accepts two numbers and return smaller number
 min = lambda num1, num2 : num1 if num1 < num2 else num2
 print("Smaller number is : ", min(10, 20))
 print("Smaller number is : ", min(130, 20))
 
 
-
+# 5. write a lambda function which accepts one number and check whether that number is even or not
 isEven = lambda num : True if num % 2 == 0 else False
 print("Is even:", isEven(4))
 print("Is even:", isEven(7))
 
-
+# 6. write a lambda function which accepts one number and check whether that number is odd or not
 isOdd = lambda num : True if num % 2 == 1 else False
 print("Is odd:", isOdd(5))
 print("Is odd:", isOdd(6))
 
-
+#7. write a lambda function which accepts one number and check whether that number is divisible by 5 or not
 isdivisible = lambda num : True if num % 5 == 0 else False
 print("Is divisible by 5:", isdivisible(10))
 
-
+# 8. write a lambda function which accepts two numbers and return their addition
 addition = lambda num1, num2 : num1 + num2
 print("Addition is:", addition(10, 20))
 
+# 9. write a lambda function which accepts two numbers and return their multiplication
 mult = lambda num1 , num2 : num1 * num2
 print("Multiplication is:", mult(10, 20))
 
+# 10. write a lambda function which accepts three numbers and return the largest number
 largest = lambda num1, num2 , num3 :  num1  if (num1 > num2 and num1> num3) else  num2 if (num2 > num1 and num2 > num3) else num3
 print("Largest number is:", largest(10, 20, 5))
 print("Largest number is:", largest(100, 20, 5))
--- Assignment15.py
+++ Assignment15.py
@@ -1,13 +1,26 @@
-numbers = [10, 20 , 30 , 40 , 55]
+from functools import reduce
 #squares = list(map(lambda num: num * num, numbers))
 
 # def square(num):
 #     return num * num
 
 #squares = list(map(square, numbers))
+numbers = [10, 20 , 30 , 40 , 55]
+
+
+# 1 write a lambda function using map() which accepts list of numbers and return list of square of each number
 squares = list(map(lambda num : num * num, numbers))
-print(squares)
+print("list of squares:", squares)
 
 
+# 2 write a lambda function using filter() which accepts list of numbers and return list even numbers
 evenNos = list(filter(lambda num: num % 2 == 0, numbers))
 print("list of even numbers",evenNos)
+
+# 3 write a lambda function using map() which accepts list of numbers and return list of odd numbers
+oddNo = list(map(lambda num : num % 2 == 1, numbers))
+print("list of odd numbers", oddNo)
+
+# 4     write a lambda function using reduce() which accepts list of numbers and return addition of all numbers
+addition = reduce(lambda num1, num2 : num1 + num2, numbers)
+print("addition of all numbers", addition)
